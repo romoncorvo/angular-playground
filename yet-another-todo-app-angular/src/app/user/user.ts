d9cbb56c1ae457eebc7952058a2d549dd0cc192d
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2025-07-21 22:20:40 +0200

    NgContent and .component convention

--- yet-another-todo-app-angular/src/app/user/user.ts
+++ yet-another-todo-app-angular/src/app/user/user.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { UserModel } from "./user.model";
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: "app-user",
-  imports: [],
+  imports: [CardComponent],
   templateUrl: "./user.html",
   styleUrl: "./user.css",
 })
