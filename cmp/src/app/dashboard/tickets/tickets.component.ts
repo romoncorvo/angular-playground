82b677587007a9832d1b65d761db992ad05e5a4f
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2025-08-23 15:52:56 +0200

    [cmp] Split components

---
+++ cmp/src/app/dashboard/tickets/tickets.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets',
+  imports: [],
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css',
+})
+export class TicketsComponent {}
