7951b5946dc74ef1eea13c3d653e2914560c4361
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2025-08-23 18:53:33 +0200

    [cmp] Multiple ng contents

--- cmp/src/app/header/header.component.ts
+++ cmp/src/app/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
