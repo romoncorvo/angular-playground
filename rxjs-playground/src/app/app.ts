351e8d2760cb4d0ecfdd1a5aa4e7cb8175128829
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2025-08-30 13:44:11 +0200

    [playground] Change app to use npm

--- rxjs-playground/src/app/app.ts
+++ rxjs-playground/src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, linkedSignal, signal } from '@angular/core';
+
+interface ShippingMethod {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template: ``,
 })
 export class App {
-  protected readonly title = signal('rxjs-playground');
+  constructor() {
+    this.changeShipping(2);
+    this.changeShippingOptions();
+    console.log(this.selectedOption());
+  }
+
+  shippingOptions = signal<ShippingMethod[]>([
+    { id: 0, name: 'Ground' },
+    { id: 1, name: 'Air' },
+    { id: 2, name: 'Sea' },
+  ]);
+
+  selectedOption = linkedSignal<ShippingMethod[], ShippingMethod>({
+    source: this.shippingOptions,
+    computation: (newOptions, previous) => {
+      return (
+        newOptions.find((opt) => opt.name === previous?.value.name) ??
+        newOptions[0]
+      );
+    },
+  });
+
+  changeShipping(index: number) {
+    this.selectedOption.set(this.shippingOptions()[index]);
+  }
+
+  changeShippingOptions() {
+    this.shippingOptions.set([
+      { id: 0, name: 'Email' },
+      { id: 1, name: 'Sea' },
+      { id: 2, name: 'Postal Service' },
+    ]);
+  }
 }
