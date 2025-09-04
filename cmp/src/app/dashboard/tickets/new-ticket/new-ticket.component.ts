81933234bd2fb981821469b0e4298ce945c14d93
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2025-09-04 20:48:48 +0200

    [cmp] ng-content

--- cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [],
+  imports: [ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
